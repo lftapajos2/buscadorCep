067817024fce0593215c425aef46c485e0820b26
llgomes <luis.gomes@foton.la>
2023-08-25 08:09:13 -0300

    Executando busca de endereço e geração de arquivo JSON.

--- src/ConsultaCep.java
+++ src/ConsultaCep.java
@@ -1,6 +1,5 @@
 import com.google.gson.Gson;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -14,13 +13,12 @@ public class ConsultaCep {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(endereco)
                 .build();
-        HttpResponse<String> response = null;
         try {
-            response = client
+            HttpResponse<String> response = client
                     .send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
+            return new Gson().fromJson(response.body(), Endereco.class);
+        } catch (Exception e) {
             throw new RuntimeException("Endereço não encontrado à partir desse CEP.");
         }
-        return new Gson().fromJson(response.body(), Endereco.class);
     }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,25 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+
+        Scanner leitura = new Scanner(System.in);
         ConsultaCep consultaCep = new ConsultaCep();
-        Endereco novoEndereco = consultaCep.buscaEndereco("22041001");
-        System.out.println(novoEndereco);
+
+        System.out.println("Digite um CEP para consulta: ");
+        var cep = leitura.nextLine();
+
+        try {
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.salvaJson(novoEndereco);
+        } catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação");
+        }
+
     }
+
 }
